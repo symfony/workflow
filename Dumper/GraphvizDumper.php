mikeSimonson/symfony

[Workflow] Added initial set of files

src/Symfony/Component/Workflow/Dumper/GraphvizDumper.php
@@ -1,0 +1,263 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Workflow\Dumper;
+
+use Symfony\Component\Workflow\Definition;
+use Symfony\Component\Workflow\Marking;
+
+/**
+ * GraphvizDumper dumps a workflow as a graphviz file.
+ *
+ * You can convert the generated dot file with the dot utility (http://www.graphviz.org/):
+ *
+ *   dot -Tpng workflow.dot > workflow.png
+ *
+ * @author Fabien Potencier <fabien@symfony.com>
+ * @author Grégoire Pineau <lyrixx@lyrixx.info>
+ */
+class GraphvizDumper implements DumperInterface
+{
+    private static $defaultOptions = array(
+        'graph' => array('ratio' => 'compress', 'rankdir' => 'LR'),
+        'node' => array('fontsize' => 9, 'fontname' => 'Arial', 'color' => '#333333', 'fillcolor' => 'lightblue', 'fixedsize' => true, 'width' => 1),
+        'edge' => array('fontsize' => 9, 'fontname' => 'Arial', 'color' => '#333333', 'arrowhead' => 'normal', 'arrowsize' => 0.5),
+    );
+
+    /**
+     * Dumps the workflow as a graphviz graph.
+     *
+     * Available options:
+     *
+     *  * graph: The default options for the whole graph
+     *  * node: The default options for nodes (places + transitions)
+     *  * edge: The default options for edges
+     *
+     * @param Definition $definition A Definition instance
+     * @param Marking    $marking    A Marking instance
+     * @param array      $options    An array of options
+     *
+     * @return string The dot representation of the workflow
+     */
+    public function dump(Definition $definition, Marking $marking = null, array $options = array())
+    {
+        $places = $this->findPlaces($definition, $marking);
+        $transitions = $this->findTransitions($definition);
+        $edges = $this->findEdges($definition);
+
+        $options = array_replace_recursive(self::$defaultOptions, $options);
+
+        return $this->startDot($options)
+            .$this->addPlaces($places)
+            .$this->addTransitions($transitions)
+            .$this->addEdges($edges)
+            .$this->endDot();
+    }
+
+    /**
+     * Finds all places.
+     *
+     * @return array An array of all places
+     */
+    private function findPlaces(Definition $definition, Marking $marking = null)
+    {
+        $places = array();
+
+        foreach ($definition->getPlaces() as $place) {
+            $attributes = [];
+            if ($place === $definition->getInitialPlace()) {
+                $attributes['style'] = 'filled';
+            }
+            if ($marking && $marking->has($place)) {
+                $attributes['color'] = '#FF0000';
+                $attributes['shape'] = 'doublecircle';
+            }
+            $places[$place] = array(
+                'attributes' => $attributes,
+            );
+        }
+
+        return $places;
+    }
+
+    /**
+     * Finds all transitions.
+     *
+     * @return array An array of all transitions
+     */
+    private function findTransitions(Definition $definition)
+    {
+        $transitions = array();
+
+        foreach ($definition->getTransitions() as $name => $nbToken) {
+            $transitions[$name] = array(
+                'attributes' => array('shape' => 'box', 'regular' => true),
+            );
+        }
+
+        return $transitions;
+    }
+
+    /**
+     * Returns all places.
+     *
+     * @return string A string representation of all places
+     */
+    private function addPlaces(array $places)
+    {
+        $code = '';
+
+        foreach ($places as $id => $place) {
+            $code .= sprintf("  place_%s [label=\"%s\", shape=circle%s];\n",
+                $this->dotize($id),
+                $id,
+                $this->addAttributes($place['attributes'])
+            );
+        }
+
+        return $code;
+    }
+
+    /**
+     * Returns all transitions.
+     *
+     * @return string A string representation of all transitions
+     */
+    private function addTransitions(array $transitions)
+    {
+        $code = '';
+
+        foreach ($transitions as $id => $place) {
+            $code .= sprintf("  transition_%s [label=\"%s\", shape=box%s];\n",
+                $this->dotize($id),
+                $id,
+                $this->addAttributes($place['attributes'])
+            );
+        }
+
+        return $code;
+    }
+
+    private function findEdges(Definition $definition)
+    {
+        $dotEdges = array();
+
+        foreach ($definition->getTransitions() as $transition) {
+            foreach ($transition->getFroms() as $from) {
+                $dotEdges[] = array(
+                    'from' => $from,
+                    'to' => $transition->getName(),
+                    'direction' => 'from',
+                );
+            }
+            foreach ($transition->getTos() as $to) {
+                $dotEdges[] = array(
+                    'from' => $transition->getName(),
+                    'to' => $to,
+                    'direction' => 'to',
+                );
+            }
+        }
+
+        return $dotEdges;
+    }
+
+    /**
+     * Returns all edges.
+     *
+     * @return string A string representation of all edges
+     */
+    private function addEdges($edges)
+    {
+        $code = '';
+
+        foreach ($edges as$edge) {
+            $code .= sprintf("  %s_%s -> %s_%s [style=\"solid\"];\n",
+                $edge['direction'] === 'from' ? 'place' : 'transition',
+                $this->dotize($edge['from']),
+                $edge['direction'] === 'from' ? 'transition' : 'place',
+                $this->dotize($edge['to'])
+            );
+        }
+
+        return $code;
+    }
+
+    /**
+     * Returns the start dot.
+     *
+     * @return string The string representation of a start dot
+     */
+    private function startDot(array $options)
+    {
+        return sprintf("digraph workflow {\n  %s\n  node [%s];\n  edge [%s];\n\n",
+            $this->addOptions($options['graph']),
+            $this->addOptions($options['node']),
+            $this->addOptions($options['edge'])
+        );
+    }
+
+    /**
+     * Returns the end dot.
+     *
+     * @return string
+     */
+    private function endDot()
+    {
+        return "}\n";
+    }
+
+    /**
+     * Adds attributes.
+     *
+     * @param array $attributes An array of attributes
+     *
+     * @return string A comma separated list of attributes
+     */
+    private function addAttributes($attributes)
+    {
+        $code = array();
+        foreach ($attributes as $k => $v) {
+            $code[] = sprintf('%s="%s"', $k, $v);
+        }
+
+        return $code ? ', '.implode(', ', $code) : '';
+    }
+
+    /**
+     * Adds options.
+     *
+     * @param array $options An array of options
+     *
+     * @return string A space separated list of options
+     */
+    private function addOptions($options)
+    {
+        $code = array();
+        foreach ($options as $k => $v) {
+            $code[] = sprintf('%s="%s"', $k, $v);
+        }
+
+        return implode(' ', $code);
+    }
+
+    /**
+     * Dotizes an identifier.
+     *
+     * @param string $id The identifier to dotize
+     *
+     * @return string A dotized string
+     */
+    private function dotize($id)
+    {
+        return strtolower(preg_replace('/[^\w]/i', '_', $id));
+    }
+}

symfony/workflow

Dumper/GraphvizDumper.php
@@ -12,6 +12,7 @@
 namespace Symfony\Component\Workflow\Dumper;
 
 use Symfony\Component\Workflow\Definition;
+use Symfony\Component\Workflow\Marking;
 
 /**
  * GraphvizDumper dumps a workflow as a graphviz file.
@@ -21,72 +22,101 @@ use Symfony\Component\Workflow\Definition;
  *   dot -Tpng workflow.dot > workflow.png
  *
  * @author Fabien Potencier <fabien@symfony.com>
+ * @author Grégoire Pineau <lyrixx@lyrixx.info>
  */
 class GraphvizDumper implements DumperInterface
 {
-    private $nodes;
-    private $edges;
-    private $options = array(
+    private static $defaultOptions = array(
         'graph' => array('ratio' => 'compress', 'rankdir' => 'LR'),
-        'node'  => array('fontsize' => 9, 'fontname' => 'Arial', 'color' => '#333', 'shape' => 'circle', 'fillcolor' => 'lightblue', 'fixedsize' => true, 'width' => 1),
-        'edge'  => array('fontsize' => 9, 'fontname' => 'Arial', 'color' => '#333', 'arrowhead' => 'normal', 'arrowsize' => 0.5),
+        'node' => array('fontsize' => 9, 'fontname' => 'Arial', 'color' => '#333333', 'fillcolor' => 'lightblue', 'fixedsize' => true, 'width' => 1),
+        'edge' => array('fontsize' => 9, 'fontname' => 'Arial', 'color' => '#333333', 'arrowhead' => 'normal', 'arrowsize' => 0.5),
     );
 
     /**
+     * {@inheritdoc}
+     *
      * Dumps the workflow as a graphviz graph.
      *
      * Available options:
      *
      *  * graph: The default options for the whole graph
-     *  * node: The default options for nodes
+     *  * node: The default options for nodes (places + transitions)
      *  * edge: The default options for edges
-     *
-     * @param Definition $definition A Definition instance
-     * @param array      $options    An array of options
-     *
-     * @return string The dot representation of the workflow
      */
-    public function dump(Definition $definition, array $options = array())
+    public function dump(Definition $definition, Marking $marking = null, array $options = array())
     {
-        foreach (array('graph', 'node', 'edge') as $key) {
-            if (isset($options[$key])) {
-                $this->options[$key] = array_merge($this->options[$key], $options[$key]);
-            }
-        }
+        $places = $this->findPlaces($definition, $marking);
+        $transitions = $this->findTransitions($definition);
+        $edges = $this->findEdges($definition);
 
-        $this->nodes = $this->findNodes($definition);
-        $this->edges = $this->findEdges($definition);
+        $options = array_replace_recursive(self::$defaultOptions, $options);
 
-        return $this->startDot().$this->addNodes().$this->addEdges().$this->endDot();
+        return $this->startDot($options)
+            .$this->addPlaces($places)
+            .$this->addTransitions($transitions)
+            .$this->addEdges($edges)
+            .$this->endDot();
     }
 
-    /**
-     * Finds all nodes.
-     *
-     * @return array An array of all nodes
-     */
-    private function findNodes(Definition $definition)
+    private function findPlaces(Definition $definition, Marking $marking = null)
     {
-        $nodes = array();
-        foreach ($definition->getStates() as $state) {
-            $nodes[$state] = array(
-                'attributes' => array_merge($this->options['node'], array('style' => $state == $definition->getInitialState() ? 'filled' : 'solid'))
+        $places = array();
+
+        foreach ($definition->getPlaces() as $place) {
+            $attributes = array();
+            if ($place === $definition->getInitialPlace()) {
+                $attributes['style'] = 'filled';
+            }
+            if ($marking && $marking->has($place)) {
+                $attributes['color'] = '#FF0000';
+                $attributes['shape'] = 'doublecircle';
+            }
+            $places[$place] = array(
+                'attributes' => $attributes,
             );
         }
 
-        return $nodes;
+        return $places;
     }
 
-    /**
-     * Returns all nodes.
-     *
-     * @return string A string representation of all nodes
-     */
-    private function addNodes()
+    private function findTransitions(Definition $definition)
+    {
+        $transitions = array();
+
+        foreach ($definition->getTransitions() as $name => $transition) {
+            $transitions[$name] = array(
+                'attributes' => array('shape' => 'box', 'regular' => true),
+            );
+        }
+
+        return $transitions;
+    }
+
+    private function addPlaces(array $places)
     {
         $code = '';
-        foreach ($this->nodes as $id => $node) {
-            $code .= sprintf("  node_%s [label=\"%s\", shape=%s%s];\n", $this->dotize($id), $id, $this->options['node']['shape'], $this->addAttributes($node['attributes']));
+
+        foreach ($places as $id => $place) {
+            $code .= sprintf("  place_%s [label=\"%s\", shape=circle%s];\n",
+                $this->dotize($id),
+                $id,
+                $this->addAttributes($place['attributes'])
+            );
+        }
+
+        return $code;
+    }
+
+    private function addTransitions(array $transitions)
+    {
+        $code = '';
+
+        foreach ($transitions as $id => $place) {
+            $code .= sprintf("  transition_%s [label=\"%s\", shape=box%s];\n",
+                $this->dotize($id),
+                $id,
+                $this->addAttributes($place['attributes'])
+            );
         }
 
         return $code;
@@ -94,72 +124,62 @@ class GraphvizDumper implements DumperInterface
 
     private function findEdges(Definition $definition)
     {
-        $edges = array();
+        $dotEdges = array();
+
         foreach ($definition->getTransitions() as $transition) {
             foreach ($transition->getFroms() as $from) {
-                foreach ($transition->getTos() as $to) {
-                    $edges[$from][] = array(
-                        'name' => $transition->getName(),
-                        'to' => $to,
-                    );
-                }
+                $dotEdges[] = array(
+                    'from' => $from,
+                    'to' => $transition->getName(),
+                    'direction' => 'from',
+                );
+            }
+            foreach ($transition->getTos() as $to) {
+                $dotEdges[] = array(
+                    'from' => $transition->getName(),
+                    'to' => $to,
+                    'direction' => 'to',
+                );
             }
         }
 
-        return $edges;
+        return $dotEdges;
     }
 
-    /**
-     * Returns all edges.
-     *
-     * @return string A string representation of all edges
-     */
-    private function addEdges()
+    private function addEdges($edges)
     {
         $code = '';
-        foreach ($this->edges as $id => $edges) {
-            foreach ($edges as $edge) {
-                $code .= sprintf("  node_%s -> node_%s [label=\"%s\" style=\"%s\"];\n", $this->dotize($id), $this->dotize($edge['to']), $edge['name'], 'solid');
-            }
+
+        foreach ($edges as $edge) {
+            $code .= sprintf("  %s_%s -> %s_%s [style=\"solid\"];\n",
+                'from' === $edge['direction'] ? 'place' : 'transition',
+                $this->dotize($edge['from']),
+                'from' === $edge['direction'] ? 'transition' : 'place',
+                $this->dotize($edge['to'])
+            );
         }
 
         return $code;
     }
 
-    /**
-     * Returns the start dot.
-     *
-     * @return string The string representation of a start dot
-     */
-    private function startDot()
+    private function startDot(array $options)
     {
         return sprintf("digraph workflow {\n  %s\n  node [%s];\n  edge [%s];\n\n",
-            $this->addOptions($this->options['graph']),
-            $this->addOptions($this->options['node']),
-            $this->addOptions($this->options['edge'])
+            $this->addOptions($options['graph']),
+            $this->addOptions($options['node']),
+            $this->addOptions($options['edge'])
         );
     }
 
-    /**
-     * Returns the end dot.
-     *
-     * @return string
-     */
     private function endDot()
     {
         return "}\n";
     }
 
-    /**
-     * Adds attributes
-     *
-     * @param array $attributes An array of attributes
-     *
-     * @return string A comma separated list of attributes
-     */
     private function addAttributes($attributes)
     {
         $code = array();
+
         foreach ($attributes as $k => $v) {
             $code[] = sprintf('%s="%s"', $k, $v);
         }
@@ -167,16 +187,10 @@ class GraphvizDumper implements DumperInterface
         return $code ? ', '.implode(', ', $code) : '';
     }
 
-    /**
-     * Adds options
-     *
-     * @param array $options An array of options
-     *
-     * @return string A space separated list of options
-     */
     private function addOptions($options)
     {
         $code = array();
+
         foreach ($options as $k => $v) {
             $code[] = sprintf('%s="%s"', $k, $v);
         }
@@ -184,13 +198,6 @@ class GraphvizDumper implements DumperInterface
         return implode(' ', $code);
     }
 
-    /**
-     * Dotizes an identifier.
-     *
-     * @param string $id The identifier to dotize
-     *
-     * @return string A dotized string
-     */
     private function dotize($id)
     {
         return strtolower(preg_replace('/[^\w]/i', '_', $id));